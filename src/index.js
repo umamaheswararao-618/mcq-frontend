8771f084a64f2d6965a75fcbad638e514b6d06a4
umamaheswararao-618 <gumamaheswararao97@gmail.com>
2025-08-05 17:12:39 +0530

    view page change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './Components/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Optional for JS components like modals, dropdowns
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
